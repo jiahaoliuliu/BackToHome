2987d53b6254489068246b0fa547e4a614eb3894
Jiahao Liu <jiahaoliuliu@gmail.com>
2014-03-04 18:46:57 +0100

    Created the native static back

--- src/com/jiahaoliuliu/backtohome/NativeStaticBackActivity.java
+++ src/com/jiahaoliuliu/backtohome/NativeStaticBackActivity.java
@@ -1,31 +1,13 @@
 package com.jiahaoliuliu.backtohome;
 
-import com.actionbarsherlock.app.SherlockActivity;
-import com.actionbarsherlock.view.MenuItem;
-
+import android.app.Activity;
 import android.os.Bundle;
-import android.support.v4.app.NavUtils;
 
-public class NativeStaticBackActivity extends SherlockActivity {
+public class NativeStaticBackActivity extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.native_static_back_layout);
-
-		// This is not needed for the native ActionBar, but
-		// for ActionBarSherlock
-		getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-	}
-	
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-	    switch (item.getItemId()) {
-	    // Respond to the action bar's Up/Home button
-	    case android.R.id.home:
-	        NavUtils.navigateUpFromSameTask(this);
-	        return true;
-	    }
-	    return super.onOptionsItemSelected(item);
 	}
 }

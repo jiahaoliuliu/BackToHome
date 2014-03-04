95c3cd7add98fa11e5ab787f17a5402db77dbc85
Jiahao Liu <jiahaoliuliu@gmail.com>
2014-03-04 18:06:48 +0100

    Reorganized the button in the home to include buttons of non native action bar back home activities

--- src/com/jiahaoliuliu/backtohome/HomeActivity.java
+++ src/com/jiahaoliuliu/backtohome/HomeActivity.java
@@ -1,46 +1,57 @@
 package com.jiahaoliuliu.backtohome;
 
+import com.actionbarsherlock.app.SherlockActivity;
+
 import android.os.Bundle;
-import android.app.Activity;
 import android.content.Intent;
-import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
 
-public class HomeActivity extends Activity {
+public class HomeActivity extends SherlockActivity {
 
-	private Button backStaticalButton;
-	private Button backDynamicalButton;
-	
+	// Native Action bar
+	private Button nativeBackStaticalButton;
+	private Button nativeBackDynamicalButton;
+
+	// Non native action bar (ActionBarSherlock)
+	private Button nonNativeBackStaticalButton;
+	private Button nonNativeBackDynamicalButton;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_home);
-		
+
 		// Link the content
-		backStaticalButton = (Button)findViewById(R.id.backStaticalButton);
-		backStaticalButton.setOnClickListener(onClickListener);
-		
-		backDynamicalButton = (Button)findViewById(R.id.backDynamicalButton);
-		backDynamicalButton.setOnClickListener(onClickListener);
+		nativeBackStaticalButton = (Button)findViewById(R.id.nativeBackStaticalButton);
+		nativeBackStaticalButton.setOnClickListener(onClickListener);
+
+		nativeBackDynamicalButton = (Button)findViewById(R.id.nativeBackDynamicalButton);
+		nativeBackDynamicalButton.setOnClickListener(onClickListener);
+
+		nonNativeBackStaticalButton = (Button)findViewById(R.id.nonNativeBackStaticalButton);
+		nonNativeBackStaticalButton.setOnClickListener(onClickListener);
+
+		nonNativeBackDynamicalButton = (Button)findViewById(R.id.nonNativeBackDynamicalButton);
+		nonNativeBackDynamicalButton.setOnClickListener(onClickListener);
 	}
-	
+
 	private View.OnClickListener onClickListener = new View.OnClickListener() {
 
 		@Override
 		public void onClick(View v) {
 			switch (v.getId()) {
-			case R.id.backStaticalButton:
+			case R.id.nativeBackStaticalButton:
 				// Start the activity
 				Intent startBackStaticalActivityIntent
-					= new Intent(HomeActivity.this, BackStaticalActivity.class);
+					= new Intent(HomeActivity.this, NativeBackStaticalActivity.class);
 				startActivity(startBackStaticalActivityIntent);
 
 				break;
-			case R.id.backDynamicalButton:
+			case R.id.nativeBackDynamicalButton:
 				// Start the activity
 				Intent startBackDynamicalActivityIntent
-					= new Intent(HomeActivity.this, BackDynamicalActivity.class);
+					= new Intent(HomeActivity.this, NativeBackDynamicalActivity.class);
 				startActivity(startBackDynamicalActivityIntent);
 
 				break;
---
+++ src/com/jiahaoliuliu/backtohome/NativeBackDynamicalActivity.java
@@ -0,0 +1,28 @@
+package com.jiahaoliuliu.backtohome;
+
+import com.actionbarsherlock.app.SherlockActivity;
+import com.actionbarsherlock.view.MenuItem;
+
+import android.os.Bundle;
+
+public class NativeBackDynamicalActivity extends SherlockActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.back_dynamical_layout);
+
+		getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		if (item.getItemId() == android.R.id.home) {
+			// Back to the previous activity
+			onBackPressed();
+			return true;
+		} else {
+			return super.onOptionsItemSelected(item);
+		}
+	}
+}

13a9980c928ebb8f73edf0b36cfd5d8ee0689301
Jiahao Liu <jiahaoliuliu@gmail.com>
2014-05-21 01:25:24 +0200

    Set the minimum version which has the native actionbar is 3.0

--- src/com/jiahaoliuliu/backtohome/NonNativeStaticBackActivity.java
+++ src/com/jiahaoliuliu/backtohome/NonNativeStaticBackActivity.java
@@ -23,9 +23,7 @@ public class NonNativeStaticBackActivity extends SherlockActivity {
 
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
-	    switch (item.getItemId()) {
-	    // Respond to the action bar's Up/Home button
-	    case android.R.id.home:
+		if (item.getItemId() == android.R.id.home) {
 	        NavUtils.navigateUpFromSameTask(this);
 	        return true;
 	    }
